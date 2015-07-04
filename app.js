2b883c8d5d0eaded7b17f140998cf64a9763c5f8
Atul <atul_r@external.mckinsey.com>
2015-07-05 02:03:56 +0530

    Test

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/app', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
